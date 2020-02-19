98b34a140a3f9b014af55154ad60d30b5f68de3a
choongangkangnam2 <choongangkangnam2@choongang.com>
2020-02-19 09:10:55 +0900

    create board table

--- myappdb.sql
+++ myappdb.sql
@@ -4,3 +4,16 @@ memberid varchar(255) PRIMARY key,
 password varchar(255) not null
 );
 select * from member;
+
+create table board
+(
+id number GENERATED always as identity primary key,
+title varchar(255) not null,
+body varchar(255) not null,
+memberid varchar(255),
+posted date default sysdate
+);
+
+
+
+
